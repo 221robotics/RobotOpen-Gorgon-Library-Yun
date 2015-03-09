21d16f3a9ec778c587e8d4f61f5756f905c15d9d
Eric Barch <ericb@ericbarch.com>
2015-03-09 19:52:13 -0400

    setup script

--- framedbridge/README.txt
+++ framedbridge/README.txt
@@ -3,29 +3,11 @@ STEP ONE
 Use PuTTY to SSH into the Yun's IP address. Username: root, Password: arduino
 
 
-STEP ONE
---------------
-$ nano /etc/inittab
-
-In the inittab file, comment out the line beginning with ttyATH0:
-
-# ttyATH0::askfirst:/bin/ash --login
-
-
 STEP TWO
 --------------
-Copy this framedbridge folder to /root (WinSCP can be used for this with the same login details used for PuTTY)
+$ curl -sSL https://raw.githubusercontent.com/221robotics/RobotOpen-Gorgon-Library-Yun/master/framedbridge/setup_robotopen.sh | sh
 
 
 STEP THREE
 --------------
-Add this to the end of /etc/rc.local before exit 0:
-python /root/framedbridge/udp_server.py
-
-
-STEP FOUR
---------------
-$ opkg update
-$ opkg install pyserial
-
-$ reboot -f
+There is no step 3. You're done!
---
+++ framedbridge/setup_robotopen.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# RobotOpen Yun Setup Script v1.0 [03.09.15]
+# Install via curl -sSL https://raw.githubusercontent.com/221robotics/RobotOpen-Gorgon-Library-Yun/master/framedbridge/setup_robotopen.sh | sh
+
+# disable arduino bridge
+sed -i '/ttyATH0/ s?^?#?' /etc/inittab
+
+# make sure we're in the root dir
+cd /root
+
+# create framedbridge working dir
+mkdir framedbridge
+
+# change into framedbridge dir
+cd framedbridge
+
+# download framedbridge from github
+wget --no-check-certificate https://raw.githubusercontent.com/221robotics/RobotOpen-Gorgon-Library-Yun/master/framedbridge/framedbridge.py
+wget --no-check-certificate https://raw.githubusercontent.com/221robotics/RobotOpen-Gorgon-Library-Yun/master/framedbridge/udp_server.py
+
+# update package manager
+opkg update
+# install pyserial
+opkg install pyserial
+
+# start framedbridge on boot
+sed -i '/exit 0/i python /root/framedbridge/udp_server.py' /etc/rc.local
+
+# done, let's reboot this sucker
+read -p "Success! Press [Enter] to finish and reboot..."
